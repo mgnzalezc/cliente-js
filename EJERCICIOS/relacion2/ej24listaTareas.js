ad9b614d65dfaddc61ad3616bcecc7fdf6d12921
mgnzalezc <mariagnzalezc@yahoo.co.uk>
2025-11-11 14:24:46 +0100

    ej24 done

--- EJERCICIOS/relacion2/ej24listaTareas.js
+++ EJERCICIOS/relacion2/ej24listaTareas.js
@@ -6,10 +6,28 @@ window.onload = () => {
     
     aPpio.onclick = ()=>{
         let nuevoLi = document.createElement("li");
-        let texto = prompt("Añade texto al final:");
+        let texto = document.createTextNode(prompt("Añade texto al principio:"));
         nuevoLi.appendChild(texto);
-        //seguir aqui
+        let lista = document.querySelector("ul");
+        lista.insertBefore(nuevoLi, lista.firstChild); //porque no va lista.elements[0]? porque elements es solo para form
     }
-    
 
+    aFinal.onclick = ()=>{
+        let nuevoLi = document.createElement("li");
+        let texto = document.createTextNode(prompt("Añade texto al final:"));
+        nuevoLi.appendChild(texto);
+        let lista = document.querySelector("ul");
+        lista.appendChild(nuevoLi);
+    }
+
+    bPpio.onclick = ()=>{
+        let aBorrar = document.querySelectorAll("li")[0];
+        aBorrar.parentNode.removeChild(aBorrar); 
+    }
+
+    bFinal.onclick = ()=>{
+        let lista = document.querySelectorAll("li");
+        let aBorrar = lista[lista.length-1];
+        aBorrar.parentNode.removeChild(aBorrar); 
+    }
 }
