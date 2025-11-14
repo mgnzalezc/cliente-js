57cc508a18854f48e5926963e27b031d9e9a1174
mgnzalezc <mariagnzalezc@yahoo.co.uk>
2025-11-14 14:20:22 +0100

    adivina

--- EJERCICIOS/relacion2/minjuegos/adivina.js
+++ EJERCICIOS/relacion2/minjuegos/adivina.js
@@ -1,8 +1,24 @@
 window.onload = ()=>{
     let inicio = document.querySelector("img");
+    let apuesta = document.getElementsByTagName("apuesta");
+    let rindo = document.getElementsByTagName("rendirse");
+    let jugar = document.getElementsByTagName("jugar");
+
+    
     inicio.onclick =()=> {
         let rdm = parseInt(Math.random()*(100)+1);
-        
+        jugar.disabled = false;
+        rindo.disabled = false;
+        inicio.disabled = true;
+
+        //dowhile apuesta q este en rango
+        if(apuesta == rdm){
+
+        }
+
+
+
+
     }
 
 
