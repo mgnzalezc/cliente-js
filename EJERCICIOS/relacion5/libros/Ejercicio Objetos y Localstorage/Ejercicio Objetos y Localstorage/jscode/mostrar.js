6a1579b2d2dfdf3545e85f194bf68e6841d603c3
mgnzalezc <mariagnzalezc@yahoo.co.uk>
2026-01-23 14:06:04 +0100

    json

--- EJERCICIOS/relacion5/libros/Ejercicio Objetos y Localstorage/Ejercicio Objetos y Localstorage/jscode/mostrar.js
+++ EJERCICIOS/relacion5/libros/Ejercicio Objetos y Localstorage/Ejercicio Objetos y Localstorage/jscode/mostrar.js
@@ -1,41 +1,51 @@
 window.onload = ()=>{
     let libro = JSON.parse(sessionStorage.getItem("libroMostrar"));
-    console.log(libro);
-    console.log(libro.key);
-    let table = document.createElement("table")
-    let info = document.querySelector("#info")
 
+    // cambiamos enlace
+    let enlace = document.querySelector("footer a").href="inicio.html";
+    // cogemos padre
+    let info = document.querySelector("#info");
+    
+    // TITULO
+    let titulo = document.createElement("h3");
+    titulo.textContent = libro.title;
+    info.appendChild(titulo);
+
+
+    // TABLA
+    let table = document.createElement("table");
     info.appendChild(table);
 
-
     for (const key in libro) {
-        
-        let tr = document.createElement("tr")
-        let tdI = document.createElement("td")
-        let tdD = document.createElement("td")
-        
+        if (key != "cover" && key!= "title"){
+            let tr = document.createElement("tr");
+            let tdI = document.createElement("td");
+            let tdD = document.createElement("td");
+                
+            if(key == "author"){
+                tdI.textContent = key;
+                tdD.textContent = libro[key]["name"];
+            }else{
+                tdI.textContent = key;
+                tdD.textContent = libro[key];
+            }
+
+            tr.appendChild(tdI);
+            tr.appendChild(tdD);
+
+            table.appendChild(tr);
+
             
-        if(key == "author"){
-            tdI.textContent = key 
-            tdD.textContent = libro[key]["name"] 
-        }else{
-            tdI.textContent = key 
-            tdD.textContent = libro[key]
+        } 
+        // FOTO
+        else if(key == "cover"){
+            let foto = document.createElement("img");
+            foto.setAttribute("src", libro['cover']);
+            foto.setAttribute("alt", "foto portada");
+            info.appendChild(foto);
         }
-
-        tr.appendChild(tdI)
-        tr.appendChild(tdD)
-
-        table.appendChild(tr)
-
-
-        
         
     }
-
-
-    
-
 
     
 }
